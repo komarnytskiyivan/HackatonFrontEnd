14fe2db548c49c3d07caf4216e1f1117abdbaefe
komarnytskiyivan <komar.ivan.97@gmail.com>
2020-12-19 14:12:43 +0200

    Added structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import  Home  from "./components/pages/Home/Home";
+import  Courier  from "./components/pages/Courier/Courier";
+import  Customer  from "./components/pages/Customer/Customer";
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact component={Home}/>
+        <Route path='/Courier' exact component={Courier}/>
+        <Route path='/Customer' exact component={Customer}/>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/pages/Customer/Customer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+function Customer() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Customer
