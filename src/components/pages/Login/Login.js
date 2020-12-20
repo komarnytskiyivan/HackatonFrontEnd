efceee1e38a6956b76240f800bc86bef366715fe
currentlib <y.tymkiv.official@gmail.com>
2020-12-20 05:10:03 +0200

    fixed css login form

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -6,12 +6,12 @@ function Login() {
             <div className='customer__hero-section whiteBg'>
             <div className="container">
                 <div className="flex-main">
-                <div className="login-form page">
+                <div className="login-form">
                     <p>Вхід</p>
                     <label for="login field field_v1">Ваш e-mail</label>
-                    <input type="text" name="username" id="login" className="input-text omrs-input-underlined omrs-input-danger" placeholder="pipipoopoo@gmail.com"/>
+                    <input type="text" name="username" id="login" className="input-text" placeholder="pipipoopoo@gmail.com"/>
                     <label for="login">Ваш пароль</label>
-                    <input type="text" name="username" id="login" className="input-text form__field" placeholder="Введіть пароль"/>
+                    <input type="text" name="username" id="login" className="input-text" placeholder="Введіть пароль"/>
                     <button type="submit" className="login-button">Вхід</button>
                 </div>
                 <div className="login-img">
